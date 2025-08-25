7630f51701747c661a929eba37a5aceab4e1ab10
Patrick Kernan <pkernan18@yahoo.com>
2025-08-25 10:40:07 -0400

    update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -19,11 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1u2!y1+82+na#&=vxasma%ibh6)p1^vfn%7-8pz16sp$*j-07p'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
